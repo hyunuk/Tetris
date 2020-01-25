c152ed552f2f5ed9d12ea083eaa42ec906e52235
I519061 <hyunuk.lim@sap.com>
2020-01-25 14:59:17 -0800

    Complete the basic logic

---
+++ Assets/SpawnTetromino.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnTetromino : MonoBehaviour {
+    public GameObject[] Tetrominoes;
+
+    void Start() {
+        NewTetromino(); 
+    }
+
+    // Update is called once per frame
+    public void NewTetromino() {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
